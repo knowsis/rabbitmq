26326f630500b3257d8650646c507c34e7ad18eb
Mark Unsworth <mark@knows.is>
2016-02-16 16:54:45 +0000

    removed sensitive flag to be compatable with chef 11.10

--- providers/user.rb
+++ providers/user.rb
@@ -89,8 +89,7 @@ action :add do
     # of leaning toothpicks:
     new_password = new_resource.password.gsub("'", "'\\\\''")
     cmd = "rabbitmqctl add_user #{new_resource.user} '#{new_password}'"
-    execute "rabbitmqctl add_user #{new_resource.user}" do # ~FC009
-      sensitive true
+    execute "rabbitmqctl add_user #{new_resource.user}" do # ~FC009      
       command cmd
       Chef::Log.info "Adding RabbitMQ user '#{new_resource.user}'."
     end
@@ -165,8 +164,7 @@ end
 action :change_password do
   if user_exists?(new_resource.user)
     cmd = "rabbitmqctl change_password #{new_resource.user} #{new_resource.password}"
-    execute "rabbitmqctl change_password #{new_resource.user}" do # ~FC009
-      sensitive true
+    execute "rabbitmqctl change_password #{new_resource.user}" do # ~FC009      
       command cmd
       Chef::Log.debug "rabbitmq_user_change_password: #{cmd}"
       Chef::Log.info "Editing RabbitMQ user '#{new_resource.user}'."
